dff4f9408620d9cabcd992db1ad82a92d099f2b4
meuNobre <nobrecoding@gmail.com>
2025-12-20 10:44:38 -0300

    V0.8.2 - Testes Discloud

--- backend/app.py
+++ backend/app.py
@@ -505,6 +505,5 @@ def get_dashboard_stats():
 
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
